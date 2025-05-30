985bd73bd8684400b6601bf954478fe504fd3d72
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-05-30 15:50:28 +0200

    Handle email validation with js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
 import "./modern-normalize.css";
 import "./styles.css";
+
+const form = document.querySelector("form");
+const email = document.querySelector("#email");
+const emailError = document.querySelector(".email.error");
+
+email.addEventListener("input", () => {
+  emailError.textContent = "";
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (email.validity.valueMissing) {
+    emailError.textContent = "Missing email";
+  } else if (email.validity.typeMismatch) {
+    emailError.textContent = "Not an email address";
+  } else {
+    emailError.textContent = "";
+  }
+});
